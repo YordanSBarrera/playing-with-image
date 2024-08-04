997b7ec3dbf7dd384613bfd7f903bc0df575162d
Yordan Saldaña <yordansaldanabarrera@gmail.com>
2024-08-04 16:04:40 -0300

    Create FileUpload.tsx

---
+++ src/components/FileUpload.tsx
@@ -0,0 +1,36 @@
+import { Stack } from "@mui/material";
+import { useRef } from "react";
+
+type FileUploadLinkProps = {
+  onFileSelect: (file: File) => void;
+};
+
+const FileUpload = ({ onFileSelect }: FileUploadLinkProps) => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      onFileSelect(file);
+    }
+  };
+
+  const handleLinkClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  return (
+    <Stack spacing={2} alignItems="center">
+      <a onClick={handleLinkClick}>Upload Image</a>
+      <input
+        ref={fileInputRef}
+        type="file"
+        style={{ display: "none" }}
+        onChange={handleFileChange}
+        accept=".jpg,.jpeg,.png,.svg"
+      />
+    </Stack>
+  );
+};
+
+export default FileUpload;
